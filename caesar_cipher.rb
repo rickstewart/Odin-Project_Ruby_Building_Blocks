9c939cdcd29b610046edb2512aa9ea930e361e94
rickstewart <rick.stewart@ns.sympatico.ca>
2015-04-02 05:38:45 -0300

    commit 4

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,27 +1,30 @@
 class CaesarCipher
 
   def self.process_cipher(orders, offset)
-    puts "orders: #{orders}, offset: #{offset}"
     @char_array_clear_text = orders.scan(/./)
     @char_array_encrypted_text = []
     @char_array_clear_text.each do |char|
-      ascii = char.ord.to_i
-      ascii_encryped = ascii + offset
-      if ascii > 64 && ascii < 91
+      ascii_clear_text = char.ord
+      if ascii_clear_text > 64 && ascii_clear_text < 91
         is_caps = true
-      else
+        ascii_encrypted = ascii_clear_text + offset.to_i
+      elsif ascii_clear_text > 96 && ascii_clear_text < 123
         is_caps = false
+        ascii_encrypted = ascii_clear_text + offset.to_i
+      else
+        ascii_encrypted = ascii_clear_text
       end
-      if ascii_encryped > 90 && ascii_encryped < 97
-        ascii_encryped += 7
-      elsif ascii_encryped > 122
-        ascii_encryped = (ascii_encryped % 122) + 64
+      if ascii_encrypted > 90 && ascii_encrypted < 97
+        ascii_encrypted += 7
+      elsif ascii_encrypted > 122
+        ascii_encrypted = (ascii_encrypted % 122) + 63
       end
-      ascii_encryped = ascii_encryped.chr
+      ascii_encrypted = ascii_encrypted.chr
+      ascii_encrypted.downcase
       if is_caps
-        ascii_encryped.upcase
+        ascii_encrypted.upcase
       end
-      @char_array_encrypted_text << ascii_encryped
+      @char_array_encrypted_text << ascii_encrypted
     end
   end
 
@@ -48,4 +51,5 @@ class CaesarCipher
       end
     end
   end
+  puts "Sire, by your leave"
 end
