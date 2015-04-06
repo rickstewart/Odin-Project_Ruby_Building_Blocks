e5041b756c746407027dc4a614cb7b5a46593b08
rickstewart <rick.stewart@ns.sympatico.ca>
2015-04-06 18:28:25 -0300

    initial substrings

--- substrings.rb
+++ substrings.rb
@@ -5,19 +5,22 @@ class Substrings
     word_array = words_to_be_searched.split(" ")
     word_array.each do |word|
       dictionary.each do |substring|
-        if @results.include? substring do
-          value = @results[substring]
-          @results.merge! (substring value + 1)
+        if word.include? substring
+          if @results.include? substring do
+            value = @results[substring]
+            @results.merge! (substring value + 1)
           end
-        else
-          @results.store(substring, 1)
+          else
+            @results.store(substring, 1)
+          end
         end
       end
+      puts @results
     end
-    puts @results
   end
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 tests = Substrings.new
 tests.substrings("below", dictionary)
+tests.substrings("Howdy partner, sit down! How's it going?", dictionary)
