b3b0a15bff19ec49624bce3d9bc47491ad948a86
rickstewart <rick.stewart@ns.sympatico.ca>
2015-04-06 18:15:10 -0300

    initial substrings

--- substrings.rb
+++ substrings.rb
@@ -1,21 +1,23 @@
 class Substrings
-  @dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
-  @results = Hash.new
 
   def substrings(words_to_be_searched, dictionary)
+    @results = Hash.new
     word_array = words_to_be_searched.split(" ")
     word_array.each do |word|
-      @dictionary.each do |substring|
-        if word.include? substring
+      dictionary.each do |substring|
+        if @results.include? substring do
           value = @results[substring]
           @results.merge! (substring value + 1)
+          end
         else
           @results.store(substring, 1)
         end
       end
     end
+    puts @results
   end
 end
 
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 tests = Substrings.new
-tests.substrings("below", @dictionary)
+tests.substrings("below", dictionary)
