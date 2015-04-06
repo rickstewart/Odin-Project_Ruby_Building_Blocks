a4a5458b0044ed2dadf1b7712850a114f64c3271
rickstewart <rick.stewart@ns.sympatico.ca>
2015-04-06 14:32:53 -0300

    initial Stock Picker

--- README.md
+++ README.md
@@ -2,9 +2,11 @@ The Odin Project - Basic Ruby - Projects: Ruby Building Blocks
 
 http://www.theodinproject.com/ruby-programming/building-blocks?ref=lnav
 
+-------------------------------------------------------------
+
 Project 1: Caesar Cipher
 
-Class file caesar_cipher implements the basic Caesar Cipher.
+caesar_cipher.rb implements the basic Caesar Cipher.
 
 Test Data: 
 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1234567890!@#$%^&*()[]
@@ -16,4 +18,13 @@ Note: offset 0 should equal 26, 1 should equal 27, -1 should equal 25
 
 -------------------------------------------------------------
 
-Project 2 
+Project 2: Stock Picker
+
+stock_picker.rb implements a basic stock picking algorithm.
+
+Tests are included in the file.
+
+-------------------------------------------------------------
+
+Project 3:
+
--- stock_picker.rb
+++ stock_picker.rb
@@ -2,20 +2,36 @@ class StockPicker
 
   def stock_picker(daily_prices)
     @best_price_spread = 0
-    @buy_on = 0
-    @sell_on = 0
-    first = 1
+    start = 1
     last = daily_prices.length - 1
-    loop_again = true
-    while loop_again
-      daily_prices[0, last].each_with_index do |price_1, index_1|
-        daily_prices[first, last].each_with_index do |price_2, index_2|
-          if price_1 > price_2 && (price_1 - price_2) > @best_price_spread
-            @best_price_spread = price_1 - price_2
-
-          end
+    daily_prices.each_with_index do |price_1, index_1|
+      daily_prices[start, last].each_with_index do |price_2, index_2|
+        if price_2 - price_1 > @best_price_spread
+          @best_price_spread = price_2 - price_1
+          @buy_on = index_1
+          @sell_on = index_2 + start
         end
       end
+      start = start + 1
+    end
+  end
+
+  def display_results
+    if @best_price_spread == 0
+      puts "Sorry, there were no buying opportunities this time round. \n \n"
+    else
+      puts "For maximum profit, buy on #{@buy_on} and then sell on #{@sell_on}."
+      puts "Your profit was #{@best_price_spread} \n \n"
     end
   end
 end
+
+tests = StockPicker.new
+tests.stock_picker([17,3,6,9,15,8,6,1,10])
+tests.display_results
+tests.stock_picker([1,3,6,10,17,22])
+tests.display_results
+tests.stock_picker([10,9,8,7,6,5,4,3,2])
+tests.display_results
+tests.stock_picker([18,9,8,12,6,5,14,3,2])
+tests.display_results
